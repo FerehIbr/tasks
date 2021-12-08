c5ff3493f6dda64ec1d268733a600e4260f276a9
[FerehIbr] <[farahei@code.edu.az]>
2021-12-08 16:19:03 +0400

    task4,5

---
+++ tasks/tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task countword
+            //string sentence = "hello babies";
+            //int count = 1;
+            //for (int i = 0; i < sentence.Length - 1; i++)
+            //{
+            //    if (sentence[i] == ' ')
+            //    {
+            //        count++;
+
+            //    }
+
+            //}
+            #endregion
+
+            #region countThe
+
+            //Console.WriteLine(count);
+
+            //string sentence = "The string where the word the present the more than once";
+            //int count = 0;
+            //int i = 0;
+            //for (i = 0; i < sentence.Split().Length - 1; i++)
+            //{
+            //    if (sentence.Split()[i] == "The"  || sentence.Split()[i] == "the" || sentence.Split()[i] == "THe" )
+            //    {
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+
+
+
+
+
+
+
+        }
+    }
+}
