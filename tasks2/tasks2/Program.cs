93602216e7888ca12ef0326ac9f9d8461f76e2d4
FerehIbr <65222474+FerehIbr@users.noreply.github.com>
2021-12-08 16:21:45 +0400

    Add files via upload

---
+++ tasks2/tasks2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tasks2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Fereh","uc","IT","azmiu","farahei@code.edu.az",0503109060);
+            student1.Stud();
+
+            
+        }
+    }
+}
---
+++ tasks2/tasks2/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks2
+{
+    class Student
+    {
+       public string Name;
+       public string Course;
+       public string Subject;
+       public string Universty;
+       public string Email;
+       public int Phonenumber;
+
+
+        public Student(string name,string course,string subject,string universty,string email,int phonenumber)
+        {
+            this.Name = name;
+            this.Course = course;
+            this.Subject = subject;
+            this.Universty = universty;
+            this.Email = email;
+            this.Phonenumber = phonenumber;
+        }
+
+        public void Stud()
+        {
+            Console.WriteLine($"{Name}\n{Course}\n{Subject}\n{Universty}\n{Email}\n{Phonenumber}");
+        }
+
+    }
+    
+}
