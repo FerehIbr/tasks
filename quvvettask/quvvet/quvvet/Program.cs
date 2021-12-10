de97762a2c91a43f12613b79af30acdd6d79f084
FerehIbr <65222474+FerehIbr@users.noreply.github.com>
2021-12-10 15:04:57 +0400

    Add files via upload

---
+++ quvvettask/quvvet/quvvet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace quvvet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quvvet quvvet1 = new Quvvet();
+            quvvet1.Quvvetf(23);
+        }
+    }
+}
---
+++ quvvettask/quvvet/quvvet/quvvet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quvvet
+{
+    class Quvvet
+    {
+        public void Quvvetf(int f)
+        {
+            bool res = false;
+            for (int i = 1; i <f; i*=2)
+            {
+                if (f == i*2)
+                {
+                    res = true;
+                }
+                else 
+                {
+                    res = false;
+                }
+              
+            }
+            if (res)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
